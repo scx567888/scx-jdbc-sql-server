92222f347d241150ca93df015740e74b003bee0a
scx567888 <scx567888@outlook.com>
2025-04-11 16:55:44 +0800

    dev

--- src/main/java/cool/scx/jdbc/sql_server/SQLServerDialect.java
+++ src/main/java/cool/scx/jdbc/sql_server/SQLServerDialect.java
@@ -3,10 +3,12 @@ package cool.scx.jdbc.sql_server;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import cool.scx.jdbc.JDBCType;
-import cool.scx.jdbc.dialect.DDLBuilder;
 import cool.scx.jdbc.dialect.Dialect;
+import cool.scx.jdbc.type_handler.TypeHandler;
+import cool.scx.jdbc.type_handler.TypeHandlerSelector;
 
 import javax.sql.DataSource;
+import java.lang.reflect.Type;
 import java.sql.Driver;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,13 +19,16 @@ import java.sql.Statement;
  * @author scx567888
  * @version 0.0.1
  */
-public class SQLServerDialect extends Dialect {
+public class SQLServerDialect implements Dialect {
 
     private static final SQLServerDriver DRIVER = initDRIVER();
+    
+    private final TypeHandlerSelector typeHandlerSelector = new TypeHandlerSelector();
 
     private static SQLServerDriver initDRIVER() {
         return new SQLServerDriver();
     }
+    
 
     @Override
     public boolean canHandle(String url) {
@@ -54,17 +59,17 @@ public class SQLServerDialect extends Dialect {
     }
 
     @Override
-    public DDLBuilder ddlBuilder() {
-        return null;
-    }
-
-    @Override
     public DataSource createDataSource(String url, String username, String password, String[] parameters) {
         var sqlServerDataSource = new SQLServerDataSource();
         sqlServerDataSource.setURL(url);
         sqlServerDataSource.setUser(username);
         sqlServerDataSource.setPassword(password);
         return sqlServerDataSource;
+    }
+
+    @Override
+    public <T> TypeHandler<T> findTypeHandler(Type type) {
+        return typeHandlerSelector.findTypeHandler(type);
     }
 
     @Override
@@ -77,4 +82,14 @@ public class SQLServerDialect extends Dialect {
         return "";
     }
 
+    @Override
+    public String getDataTypeNameByJDBCType(JDBCType dataType) {
+        return dataType.name();
+    }
+
+    @Override
+    public String quoteIdentifier(String identifier) {
+        return "[" + identifier + "]";
+    }
+    
 }
